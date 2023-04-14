d58c6c78811c6aa5e155816a98a88dfa23666f7d
Dovidovich Anastasia <sidorova88-88@mail.ru>
2023-04-14 15:16:17 +0300

    ДЗ 2

---
+++ HW_2/task_14.py
@@ -0,0 +1,17 @@
+# Задача 14: Требуется вывести все целые степени двойки (т.е. числа
+# вида 2k
+# ), не превосходящие числа N.
+# 10 -> 1 2 4 8
+
+number = int(input("Введите число N: "))
+degree = 2
+count = 1
+print(number, degree)
+while degree <= number:
+    degree *= 2
+    count += 1
+print(count -1, degree // 2)
+
+
+
+    
